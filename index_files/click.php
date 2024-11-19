36b155d82dcb9ea4d0b34ec098195961fb20695e
TechGuru34 <cookeevie52@gmail.com>
2024-11-19 22:14:50 +0800

    Add files via upload

---
+++ index_files/click.php
@@ -0,0 +1,7 @@
+
+				var d = new Date();
+				d.setTime(d.getTime() + 3600 * 24 * 1000);
+				document.cookie = 'uclick=' + encodeURIComponent('2ta3uqir') + '; expires='+d+'; path=/';
+				
+				var tokens = new Object();
+			 tokens.uclick = "2ta3uqir"; 
